d353325a7a67d0eb4b3e5d2f278f75881771e354
Mila Halko <milagalko1@gmail.com>
2023-05-30 19:17:36 +0300

    report parallel in process

--- CourseWork/CourseWork/Program.cs
+++ CourseWork/CourseWork/Program.cs
@@ -4,8 +4,10 @@
 using System.Diagnostics;
 using BenchmarkDotNet.Running;
 using CourseWork.Benchmarks;
+using CourseWork.Graphs;
+using CourseWork.KruskalAlgorithm;
 
-BenchmarkRunner.Run<KruskalaBenchmark>();
+//BenchmarkRunner.Run<KruskalaBenchmark>();
 
 // KruskalaBenchmark benchmark = new KruskalaBenchmark();
 // benchmark.Setup();
@@ -14,3 +16,32 @@ BenchmarkRunner.Run<KruskalaBenchmark>();
 // benchmark.ParallelSortingKruskal();
 // stopwatch.Stop();
 // System.Console.WriteLine(stopwatch.ElapsedMilliseconds);
+
+Graph graph = new Graph(4);
+
+// Adding edges to the graph
+graph.AddEdge(0, 1, 10);
+graph.AddEdge(0, 2, 6);
+graph.AddEdge(0, 3, 5);
+graph.AddEdge(1, 3, 15);
+graph.AddEdge(2, 3, 4);
+
+Stopwatch stopwatch = new Stopwatch();
+stopwatch.Start();
+
+// SequentialKruskal sequentialKruskal = new SequentialKruskal();
+// sequentialKruskal.CalculateMST(graph);
+
+ParallelSortingKruskal parallelSortingKruskal = new ParallelSortingKruskal();
+List<Edge> resultPSK = parallelSortingKruskal.CalculateMST(graph);
+
+// FilterKruskal filterKruskal = new FilterKruskal();
+// filterKruskal.CalculateMST(graph);
+
+stopwatch.Stop();
+Console.WriteLine($"ParallelSortingKruskal: {stopwatch.ElapsedMilliseconds} ms");
+
+foreach(Edge edge in resultPSK)
+{
+    Console.WriteLine($"{edge.Source} -- {edge.Des tination} == {edge.Weight}");
+}
